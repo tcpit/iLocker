7998f4fbc99bd1e62e5a2b01246d97a5309dff3d
DOS <dosdong@msn.com>
2015-05-05 23:53:27 +0800

    https://github.com/tcpit/iLocker.git

---
+++ iLocker/MainForm.Designer.cs
@@ -0,0 +1,215 @@
+﻿namespace iLocker
+{
+    partial class iLockerForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(iLockerForm));
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.lblShowInfo = new System.Windows.Forms.Label();
+            this.lblPassword = new System.Windows.Forms.Label();
+            this.lblFilePath = new System.Windows.Forms.Label();
+            this.mainPanel = new System.Windows.Forms.Panel();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.listView = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.btnDecrypt = new System.Windows.Forms.Button();
+            this.btnEncrypt = new System.Windows.Forms.Button();
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.lblCopyright = new System.Windows.Forms.LinkLabel();
+            this.mainPanel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // openFileDialog
+            // 
+            this.openFileDialog.Multiselect = true;
+            // 
+            // lblShowInfo
+            // 
+            this.lblShowInfo.BackColor = System.Drawing.SystemColors.Control;
+            this.lblShowInfo.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.lblShowInfo.Location = new System.Drawing.Point(1, 4);
+            this.lblShowInfo.Name = "lblShowInfo";
+            this.lblShowInfo.Size = new System.Drawing.Size(254, 20);
+            this.lblShowInfo.TabIndex = 6;
+            this.lblShowInfo.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblPassword
+            // 
+            this.lblPassword.AutoSize = true;
+            this.lblPassword.Location = new System.Drawing.Point(11, 46);
+            this.lblPassword.Name = "lblPassword";
+            this.lblPassword.Size = new System.Drawing.Size(56, 13);
+            this.lblPassword.TabIndex = 7;
+            this.lblPassword.Text = "Password:";
+            // 
+            // lblFilePath
+            // 
+            this.lblFilePath.AutoSize = true;
+            this.lblFilePath.Location = new System.Drawing.Point(41, 10);
+            this.lblFilePath.Name = "lblFilePath";
+            this.lblFilePath.Size = new System.Drawing.Size(26, 13);
+            this.lblFilePath.TabIndex = 6;
+            this.lblFilePath.Text = "File:";
+            // 
+            // mainPanel
+            // 
+            this.mainPanel.Controls.Add(this.txtPassword);
+            this.mainPanel.Controls.Add(this.listView);
+            this.mainPanel.Controls.Add(this.btnDecrypt);
+            this.mainPanel.Controls.Add(this.btnEncrypt);
+            this.mainPanel.Controls.Add(this.btnOpen);
+            this.mainPanel.Controls.Add(this.lblFilePath);
+            this.mainPanel.Controls.Add(this.lblPassword);
+            this.mainPanel.Location = new System.Drawing.Point(2, 27);
+            this.mainPanel.Name = "mainPanel";
+            this.mainPanel.Size = new System.Drawing.Size(252, 356);
+            this.mainPanel.TabIndex = 8;
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.txtPassword.Location = new System.Drawing.Point(73, 39);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(143, 27);
+            this.txtPassword.TabIndex = 2;
+            this.txtPassword.UseSystemPasswordChar = true;
+            // 
+            // listView
+            // 
+            this.listView.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2});
+            this.listView.Location = new System.Drawing.Point(14, 71);
+            this.listView.Name = "listView";
+            this.listView.ShowItemToolTips = true;
+            this.listView.Size = new System.Drawing.Size(224, 234);
+            this.listView.TabIndex = 5;
+            this.listView.UseCompatibleStateImageBehavior = false;
+            this.listView.View = System.Windows.Forms.View.Details;
+            this.listView.KeyDown += new System.Windows.Forms.KeyEventHandler(this.listView_KeyDown);
+            this.listView.KeyUp += new System.Windows.Forms.KeyEventHandler(this.listView_KeyUp);
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "File";
+            this.columnHeader1.Width = 139;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Status";
+            this.columnHeader2.Width = 75;
+            // 
+            // btnDecrypt
+            // 
+            this.btnDecrypt.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnDecrypt.Location = new System.Drawing.Point(130, 321);
+            this.btnDecrypt.Name = "btnDecrypt";
+            this.btnDecrypt.Size = new System.Drawing.Size(80, 27);
+            this.btnDecrypt.TabIndex = 4;
+            this.btnDecrypt.Text = "Decrypt";
+            this.btnDecrypt.UseVisualStyleBackColor = true;
+            this.btnDecrypt.Click += new System.EventHandler(this.btnDecrypt_Click);
+            // 
+            // btnEncrypt
+            // 
+            this.btnEncrypt.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnEncrypt.Location = new System.Drawing.Point(44, 321);
+            this.btnEncrypt.Name = "btnEncrypt";
+            this.btnEncrypt.Size = new System.Drawing.Size(80, 27);
+            this.btnEncrypt.TabIndex = 3;
+            this.btnEncrypt.Text = "Encrypt";
+            this.btnEncrypt.UseVisualStyleBackColor = true;
+            this.btnEncrypt.Click += new System.EventHandler(this.btnEncrypt_Click);
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnOpen.Location = new System.Drawing.Point(73, 3);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(143, 27);
+            this.btnOpen.TabIndex = 1;
+            this.btnOpen.Text = "Browse";
+            this.btnOpen.UseVisualStyleBackColor = true;
+            this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
+            // 
+            // lblCopyright
+            // 
+            this.lblCopyright.AutoSize = true;
+            this.lblCopyright.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.lblCopyright.Location = new System.Drawing.Point(213, 391);
+            this.lblCopyright.Name = "lblCopyright";
+            this.lblCopyright.Size = new System.Drawing.Size(42, 13);
+            this.lblCopyright.TabIndex = 9;
+            this.lblCopyright.TabStop = true;
+            this.lblCopyright.Text = "iLocker";
+            this.lblCopyright.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.lblCopyright_LinkClicked);
+            // 
+            // iLockerForm
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(256, 411);
+            this.Controls.Add(this.lblCopyright);
+            this.Controls.Add(this.mainPanel);
+            this.Controls.Add(this.lblShowInfo);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "iLockerForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "iLocker";
+            this.DragDrop += new System.Windows.Forms.DragEventHandler(this.iLockerForm_DragDrop);
+            this.DragEnter += new System.Windows.Forms.DragEventHandler(this.iLockerForm_DragEnter);
+            this.mainPanel.ResumeLayout(false);
+            this.mainPanel.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
+        private System.Windows.Forms.Label lblShowInfo;
+        private System.Windows.Forms.Label lblPassword;
+        private System.Windows.Forms.Label lblFilePath;
+        private System.Windows.Forms.Panel mainPanel;
+        private System.Windows.Forms.Button btnDecrypt;
+        private System.Windows.Forms.Button btnEncrypt;
+        private System.Windows.Forms.Button btnOpen;
+        private System.Windows.Forms.ListView listView;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.LinkLabel lblCopyright;
+    }
+}
+
---
+++ iLocker/MainForm.cs
@@ -0,0 +1,257 @@
+﻿using System;
+using System.Data;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.ComponentModel;
+using System.Security.Cryptography;
+
+namespace iLocker
+{
+    public partial class iLockerForm : Form
+    {
+        Encoding defaultEncoding = Encoding.UTF8;
+        BackgroundWorker encryptWorker, decryptWorker;
+
+        public iLockerForm()
+        {
+            InitializeComponent();
+            encryptWorker = new BackgroundWorker();
+            encryptWorker.DoWork += new DoWorkEventHandler(Encrypt);
+            encryptWorker.RunWorkerCompleted += new RunWorkerCompletedEventHandler(EncryptionCompleted);
+
+
+            decryptWorker = new BackgroundWorker();
+            decryptWorker.DoWork += new DoWorkEventHandler(Decrypt);
+            decryptWorker.RunWorkerCompleted += new RunWorkerCompletedEventHandler(DecryptionCompleted);
+        }
+        private void Encrypt(object sender, DoWorkEventArgs e)
+        {
+            this.Invoke(new MethodInvoker(delegate()
+            {
+                string sInfo = "Encrypted";
+                foreach (ListViewItem item in listView.Items)
+                {
+                    string sFilePath = item.ToolTipText;
+                    string sKey = txtPassword.Text.Trim();
+
+                    if (File.Exists(sFilePath) == false)
+                        continue;
+
+                    if (item.SubItems.Count > 1 && item.SubItems[1].Text == sInfo)
+                        continue;
+
+                    RijndaelManaged AES = new RijndaelManaged();
+                    AES.Key = MD5(SHA(sKey));
+                    AES.IV = MD5(SHA(MD5(sKey)));
+
+                    FileStream fsCrypt = new FileStream(sFilePath, FileMode.Open, FileAccess.Write, FileShare.ReadWrite);
+
+                    CryptoStream cs = new CryptoStream(fsCrypt, AES.CreateEncryptor(), CryptoStreamMode.Write);
+
+                    FileStream fsIn = new FileStream(sFilePath, FileMode.Open, FileAccess.Read, FileShare.ReadWrite);
+
+                    int data;
+                    while ((data = fsIn.ReadByte()) != -1)
+                        cs.WriteByte((byte)data);
+
+
+                    fsIn.Close();
+                    cs.Close();
+                    fsCrypt.Close();
+
+                    if (item.SubItems.Count <= 1)
+                        item.SubItems.Add(sInfo);
+                    else
+                        item.SubItems[1].Text = sInfo;
+
+                }
+            
+            }));
+
+          
+        }
+
+        private void Decrypt(object sender, DoWorkEventArgs e)
+        {
+            this.Invoke(new MethodInvoker(delegate()
+            {
+                string sInfo = "Decrypted";
+                foreach (ListViewItem item in listView.Items)
+                {
+                    string sFilePath = item.ToolTipText;
+                    string sKey = txtPassword.Text.Trim();
+
+                    if (File.Exists(sFilePath) == false)
+                        continue;
+
+                    if (item.SubItems.Count > 1 && item.SubItems[1].Text == sInfo)
+                        continue;
+
+                    RijndaelManaged AES = new RijndaelManaged();
+                    AES.Key = MD5(SHA(sKey));
+                    AES.IV = MD5(SHA(MD5(sKey)));
+
+                    FileStream fsCrypt = new FileStream(sFilePath, FileMode.Open, FileAccess.Read, FileShare.ReadWrite);
+
+                    CryptoStream cs = new CryptoStream(fsCrypt, AES.CreateDecryptor(), CryptoStreamMode.Read);
+
+                    FileStream fsOut = new FileStream(sFilePath, FileMode.Open, FileAccess.Write, FileShare.ReadWrite);
+
+                    int data;
+                    while ((data = cs.ReadByte()) != -1)
+                        fsOut.WriteByte((byte)data);
+
+                    fsOut.Close();
+                    cs.Close();
+                    fsCrypt.Close();
+
+                    if (item.SubItems.Count <= 1)
+                        item.SubItems.Add(sInfo);
+                    else
+                        item.SubItems[1].Text = sInfo;
+
+                }
+            }));
+        }
+
+        private byte[] MD5(string sPlainText)
+        {
+            MD5CryptoServiceProvider md5 = new MD5CryptoServiceProvider();
+            return md5.ComputeHash(defaultEncoding.GetBytes(sPlainText));
+        }
+
+        private byte[] MD5(byte[] sPlainText)
+        {
+            MD5CryptoServiceProvider md5 = new MD5CryptoServiceProvider();
+            return md5.ComputeHash(sPlainText);
+        }
+        private byte[] SHA(string sPlainText)
+        {
+            SHA256Managed sha = new SHA256Managed();
+            return sha.ComputeHash(defaultEncoding.GetBytes(sPlainText));
+        }
+
+        private byte[] SHA(byte[] sPlainText)
+        {
+            SHA256Managed sha = new SHA256Managed();
+            return sha.ComputeHash(sPlainText);
+        }
+
+
+        private void btnEncrypt_Click(object sender, EventArgs e)
+        {
+            if(txtPassword.Text.Trim() == "")
+            {
+                MessageBox.Show("Please set password first!", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            lblShowInfo.Text = "Encryption Running...";
+            mainPanel.Enabled = false;
+
+
+            encryptWorker.RunWorkerAsync();
+        }
+
+        private void btnDecrypt_Click(object sender, EventArgs e)
+        {
+            if (txtPassword.Text.Trim() == "")
+            {
+                MessageBox.Show("Please set password first!", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+            
+            lblShowInfo.Text = "Decryption Running...";
+            mainPanel.Enabled = false;
+
+            decryptWorker.RunWorkerAsync();
+        }
+
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+            DialogResult re = openFileDialog.ShowDialog();
+            if (re == System.Windows.Forms.DialogResult.OK)
+            {
+                foreach (string file in openFileDialog.FileNames)
+                {
+                    if (File.Exists(file) && listView.Items.ContainsKey(file) == false)
+                    {
+                        listView.Items.Add(file, Path.GetFileName(file), file).ToolTipText = file;
+                    }                    
+                }
+            }
+        }
+
+        private void EncryptionCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            lblShowInfo.Text = "";
+            mainPanel.Enabled = true;
+        }
+
+        private void DecryptionCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            lblShowInfo.Text = "";
+            mainPanel.Enabled = true;
+        }
+
+        private void iLockerForm_DragDrop(object sender, DragEventArgs e)
+        {
+            string[] files = (string[])e.Data.GetData(DataFormats.FileDrop, false);
+            foreach (string file in files)
+            {
+                if (File.Exists(file) && listView.Items.ContainsKey(file) == false)
+                {
+                    listView.Items.Add(file, Path.GetFileName(file), file).ToolTipText = file;
+                }
+            }
+        }
+
+        private void iLockerForm_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                e.Effect = DragDropEffects.Copy;
+            }
+            else
+            {
+                e.Effect = DragDropEffects.None;
+            }
+        }
+
+        private void listView_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Delete || e.KeyCode == Keys.Back)
+            {
+                if (listView.SelectedItems.Count > 0)
+                {
+                    int lastSelectedItemIdx = 0;
+                    foreach (ListViewItem item in listView.SelectedItems)
+                    {
+                        lastSelectedItemIdx = item.Index;
+                        listView.Items.Remove(item);
+                    }
+                    if (listView.Items.Count > 0 && ( lastSelectedItemIdx < 0 || lastSelectedItemIdx >= listView.Items.Count))
+                    {
+                        lastSelectedItemIdx = 0;
+                        listView.Items[lastSelectedItemIdx].Selected = true;
+                    }
+                }
+            }
+        }
+
+        private void listView_KeyDown(object sender, KeyEventArgs e)
+        {
+            if( e.Control && e.KeyCode == Keys.A)
+            {
+                foreach (ListViewItem item in listView.Items)
+                    item.Selected = true;
+            }
+        }
+
+        private void lblCopyright_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            MessageBox.Show("iLocker © 2015\nYuv.me", "About", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+    }
+}
